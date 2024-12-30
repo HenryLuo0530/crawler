ab1ed887d4ef2e0ffc4cecff4ed92f41dd3334a5
HenryLuo0530 <henryluo.tw@gmail.com>
2024-12-31 00:05:17 +0800

    extract different mode functions to make code cleaner

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print("[S] Bot is online")
     channel = await bot.fetch_channel(setting["CHANNEL_ID"])
-    await channel.send(quote["Bot_awake"])
+    await channel.send(quote["bot_awake"])
 
 """
 @bot.event
@@ -46,107 +46,25 @@ async def seeing(ctx, limit_days="3", mode="1", latitude="25.17", longitude="121
     except:
         input_status_code = 10 
     if input_status_code > 0:
-        await ctx.send(quote["Seeing_input_status_code"][input_status_code])
+        await ctx.send(quote["seeing_input_status_code"][input_status_code])
         return
 
     crawler_ststus_code = crawler.seeing_crawl(limit_days, latitude, longitude)
     if crawler_ststus_code > 0:
-        await ctx.send(quote["Seeing_crawler_status_code"][1])
+        await ctx.send(quote["seeing_crawler_status_code"][1])
         return
     
     with open("seeings.json", "r", encoding="utf-8") as file:
         data = json.load(file)
-
-    if mode == 0:
-        day_and_dates = []
-        time_and_qualities = []
-        for d in data:
-            day_and_dates.append(d["dates"])
-            time_and_qualities += d["time_and_quality"]
-        
-        current_day = 0
-        head_day = 0
-
-        head_time = 0
-        previous_time = 0
-
-        current_max_hour = 0
-
-        is_first = True
-        is_continue = False
-
-        find_list = []
-        for tq in time_and_qualities:
-            time_and_quality = tq.split(" ")
-            time = int(time_and_quality[1])
-            quality = time_and_quality[2]
-            if (quality == "Good") and (not (6 <= time <= 17)):
-                if is_first:
-                    head_day = current_day
-                    head_time = time
-                    is_first = False
-                previous_time = time
-                current_max_hour += 1
-                is_continue = True
-            else:
-                if is_continue:
-                    find_dict = {}
-                    find_dict["start_day"] = day_and_dates[head_day]
-                    find_dict["end_day"] = day_and_dates[current_day]
-                    find_dict["start_time"] = head_time
-                    find_dict["end_time"] = previous_time
-                    find_dict["max_hour"] = current_max_hour
-                    find_list.append(find_dict)
-                    current_max_hour = 0
-                    is_first = True
-                    is_continue = False
-                else:
-                    pass
-            
-            if (time == 23):
-                current_day += 1
-        
-        if is_continue:
-            find_dict = {}
-            find_dict["start_day"] = day_and_dates[head_day]
-            find_dict["end_day"] = day_and_dates[current_day - 1]
-            find_dict["start_time"] = head_time
-            find_dict["end_time"] = previous_time
-            find_dict["max_hour"] = current_max_hour
-            find_list.append(find_dict)
-
-        if not find_list:
-            rand = random.randint(0, 1)
-            match rand:
-                case 0:
-                    await ctx.send("Oh no! Migu canNOT find good hours!")
-                case 1:
-                    await ctx.send("Oh wow! Migu see beautiful clouds!")
-        else:
-            for info in find_list:
-                msg = f'{tools.dayTranslation(info["start_day"])} {info["start_time"]}:00 ~ {tools.dayTranslation(info["end_day"])} {info["end_time"]}:00 max {info["max_hour"]}hr'
-                await ctx.send(msg)
-        return
-
-    elif mode == 1:
-        for d in data:
-            translated_day_date = tools.dayTranslation(d["dates"])
-            await ctx.send(translated_day_date)
-            time_table = "=> 00    01   02    03   04   05   06    07   08   09   10     11     12    13    14    15    16     17    18    19    20    21    22   23"
-            await ctx.send(time_table)
-            msg = "=> "
-            for tq in d["time_and_quality"]:
-                time_and_quality = tq.split(" ")
-                time = time_and_quality[1]
-                quality = time_and_quality[2]
-                if quality == "Bad":
-                    msg += ":red_circle:  "
-                elif quality == "OK":
-                    msg += ":orange_circle:  "
-                else:
-                    msg += ":green_circle:  "
-            await ctx.send(msg)
-        return
+    match mode:
+        case 0:
+            message_list = tools.find_max_time(data)
+            for message in message_list:
+                await ctx.send(message)
+        case 1:
+            message_list = tools.print_time_table(data)
+            for message in message_list:
+                await ctx.send(message)
 
 @bot.command()
 async def coin(ctx):
@@ -154,7 +72,6 @@ async def coin(ctx):
     if coin:
         coin_head = discord.File('./image/coin_head.jpg')
         await ctx.send(file = coin_head)
-            
     else:
         coin_tail = discord.File('./image/coin_tail.jpg')
         await ctx.send(file = coin_tail)
--- tools.py
+++ tools.py
@@ -1,4 +1,6 @@
-def dayTranslation(day_and_date: str) -> str:
+from random import randint
+
+def day_translation(day_and_date: str) -> str:
     dict = {
         "Monday"   : "星期一",
         "Tuesday"  : "星期二",
@@ -15,7 +17,7 @@ def dayTranslation(day_and_date: str) -> str:
     msg = translated_day + " " + date
     return msg
 
-def check_input(limit_days, mode, latitude, longitude) -> int:
+def check_input(limit_days: int, mode: int, latitude: float, longitude: float) -> int:
     if (limit_days < 1):
         return 1
     elif (limit_days > 7):
@@ -25,3 +27,97 @@ def check_input(limit_days, mode, latitude, longitude) -> int:
         return 3
     
     return 0
+
+def find_max_time(data: list) -> list:
+    day_and_dates = []
+    time_and_qualities = []
+    for d in data:
+        day_and_dates.append(d["dates"])
+        time_and_qualities += d["time_and_quality"]
+    
+    current_day, head_day = 0, 0
+    head_time, previous_time = 0, 0
+    current_max_hour = 0
+    is_first = True
+    is_continue = False
+
+    find_list = []
+    for tq in time_and_qualities:
+        time_and_quality = tq.split(" ")
+        time = int(time_and_quality[1])
+        quality = time_and_quality[2]
+        if (quality == "Good") and (not (6 <= time <= 17)):
+            if is_first:
+                head_day = current_day
+                head_time = time
+                is_first = False
+            previous_time = time
+            current_max_hour += 1
+            is_continue = True
+        else:
+            if is_continue:
+                find_dict = {}
+                find_dict["start_day"] = day_and_dates[head_day]
+                find_dict["end_day"] = day_and_dates[current_day]
+                find_dict["start_time"] = head_time
+                find_dict["end_time"] = previous_time
+                find_dict["max_hour"] = current_max_hour
+                find_list.append(find_dict)
+                current_max_hour = 0
+                is_first = True
+                is_continue = False
+            else:
+                pass
+
+        if time == 23:
+            current_day += 1
+    
+    if is_continue:
+        find_dict = {}
+        find_dict["start_day"] = day_and_dates[head_day]
+        find_dict["end_day"] = day_and_dates[current_day - 1]
+        find_dict["start_time"] = head_time
+        find_dict["end_time"] = previous_time
+        find_dict["max_hour"] = current_max_hour
+        find_list.append(find_dict)
+
+    message_list = []
+    if not find_list:
+        data_not_found_message = [
+            "Oh no! Migu canNOT find good hours!",
+            "Oh wow! Migu see beautiful clouds!"
+        ]
+        choose_message = randint(0, 1)
+        message_list.append(data_not_found_message[choose_message])
+    else:
+        for info in find_list:
+            translated_start_day = day_translation(info["start_day"])
+            translated_end_day = day_translation(info["end_day"])
+            max_time_message = f"{translated_start_day} {info["start_time"]}:00 ~ {translated_end_day} {info["end_time"]}:00 max {info["max_hour"]}hr"
+            message_list.append(max_time_message)
+    return message_list
+
+def print_time_table(data: list) -> list:
+    message_list = []
+    for d in data:
+        one_day_message = []
+        translated_day = day_translation(d["dates"])
+        one_day_message.append(translated_day)
+
+        time_table = "=> 00    01   02    03   04   05   06    07   08   09   10     11     12    13    14    15    16     17    18    19    20    21    22   23"
+        one_day_message.append(time_table)
+
+        quality_table = "=> "
+        for tq in d["time_and_quality"]:
+            time_and_quality = tq.split(" ")
+            time = time_and_quality[1]
+            quality = time_and_quality[2]
+            if quality == "Bad":
+                quality_table += ":red_circle:  "
+            elif quality == "OK":
+                quality_table += ":orange_circle:  "
+            else:
+                quality_table += ":green_circle:  "
+        one_day_message.append(quality_table)
+        message_list += one_day_message
+    return message_list
