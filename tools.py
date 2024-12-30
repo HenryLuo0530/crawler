a97bb3b4b9ff0cf6d9fa9d219a59242c3a3b8f76
HenryLuo0530 <henryluo.tw@gmail.com>
2024-12-31 01:22:38 +0800

    time table now showed by emoji

--- tools.py
+++ tools.py
@@ -1,7 +1,7 @@
 from random import randint
 
 def day_translation(day_and_date: str) -> str:
-    dict = {
+    day_dictionary = {
         "Monday"   : "星期一",
         "Tuesday"  : "星期二",
         "Wednesday": "星期三",
@@ -13,7 +13,7 @@ def day_translation(day_and_date: str) -> str:
     time_list = day_and_date.split(" ")
     day = time_list[0]
     date = time_list[1]
-    translated_day = dict[day]
+    translated_day = day_dictionary[day]
     msg = translated_day + " " + date
     return msg
 
@@ -97,20 +97,54 @@ def find_max_time(data: list) -> list:
             message_list.append(max_time_message)
     return message_list
 
+def hour_translation(hour: int) -> str:
+    number_dictionary = {
+        0: ":zero:",
+        1: ":one:",
+        2: ":two:",
+        3: ":three:",
+        4: ":four:",
+        5: ":five:",
+        6: ":six:",
+        7: ":seven:",
+        8: ":eight:",
+        9: ":nine:",
+        10: ":keycap_ten:",
+        11: "<:eleven:1323325078946975845>",
+        12: "<:twelve:1323325154528464977>",
+        13: "<:thirteen:1323325315170304010>",
+        14: "<:fourteen:1323325556980318219>",
+        15: "<:fifteen:1323325893942181969>",
+        16: "<:sixteen:1323325957502930977>",
+        17: "<:seventeen:1323326016072061008>",
+        18: "<:eighteen:1323326094346162206>",
+        19: "<:nineteen:1323326149945851964>",
+        20: "<:twenty:1323326226017816657>",
+        21: "<:twenty_one:1323326290425544725>",
+        22: "<:twenty_two:1323326382830256159>",
+        23: "<:twenty_three:1323326503060242433>"
+    }
+    translated_hour = number_dictionary[hour]
+    return translated_hour
+
 def print_time_table(data: list) -> list:
     message_list = []
     for d in data:
         one_day_message = []
         translated_day = day_translation(d["dates"])
         one_day_message.append(translated_day)
+        # source: https://emoji.gg/pack/4123-keycap-emoji-11-to-42#
+        time_table = ["=>"]
+        for hour in range(0, 24):
+            translated_hour = hour_translation(hour)
+            time_table.append(translated_hour)
+        time_message = "  ".join(time_table)
+        one_day_message.append(time_message)
 
-        time_table = "=> 00    01   02    03   04   05   06    07   08   09   10     11     12    13    14    15    16     17    18    19    20    21    22   23"
-        one_day_message.append(time_table)
-
-        quality_table = "=> "
+        quality_table = "=>  "
         for tq in d["time_and_quality"]:
             time_and_quality = tq.split(" ")
-            time = time_and_quality[1]
+            # time = time_and_quality[1]
             quality = time_and_quality[2]
             if quality == "Bad":
                 quality_table += ":red_circle:  "
