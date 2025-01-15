3882e15ce3ffdba8a57cc1d647e5be07b3b96d61
HenryLuo0530 <henryluo.tw@gmail.com>
2025-01-15 17:45:37 +0800

    add question feature

---
+++ cogs/question.py
@@ -0,0 +1,42 @@
+import platform
+from datetime import datetime
+import discord
+from discord.ext import commands
+
+from extension import tools
+
+class Question(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def who(self, ctx):
+        quote = tools.get_quote()
+        await ctx.send(quote["question"]["who"])
+
+    @commands.command()
+    async def what(self, ctx):
+        quote = tools.get_quote()
+        await ctx.send(quote["question"]["what"])
+
+    @commands.command()
+    async def when(self, ctx):
+        now = datetime.now().strftime("%Y %m/%d %H:%M:%S")
+        await ctx.send(f"{now}")
+
+    @commands.command()
+    async def where(self, ctx):
+        machine = platform.platform()
+        quote = tools.get_quote()
+        message = " ".join([
+            quote["question"]["where"][0], machine, quote["question"]["where"][1]
+        ])
+        await ctx.send(message)
+
+    @commands.command()
+    async def why(self, ctx):
+        quote = tools.get_quote()
+        await ctx.send(quote["question"]["why"])
+
+async def setup(bot):
+    await bot.add_cog(Question(bot))
