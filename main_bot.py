53036af138480b981390abc36efd135a9a7227b7
HenryLuo0530 <henryluo.tw@gmail.com>
2025-01-22 16:52:16 +0800

    add logger

--- main_bot.py
+++ main_bot.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 import discord
 from discord.ext import commands
 import asyncio
@@ -7,13 +9,14 @@ from extension import tools
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+logger = logging.getLogger(__name__)
 
 @bot.event
 async def on_ready():
     setting = tools.get_setting()
     quote = tools.get_quote()
-    print("[I] Bot is online")
-    print(f"[I] Using {setting["Language"]} as bot language")
+    logger.info("[I] Bot is online")
+    logger.info(f"[I] Using {setting["Language"]} as bot language")
     channel = await bot.fetch_channel(setting["CHANNEL_ID"])
     await channel.send(quote["on_ready"])
 
@@ -23,6 +26,23 @@ async def load():
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
 async def main():
+    logger.setLevel(logging.INFO)
+    handler = RotatingFileHandler(
+        filename="log.log",
+        mode='a',
+        maxBytes=1024 * 4,
+        backupCount=0,
+        encoding='utf-8',
+        delay=False
+    )
+    handler.setLevel(logging.INFO)
+    logging.basicConfig(
+        format="%(name)s %(asctime)s %(levelname)s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[handler]
+    )
+
     setting = tools.get_setting()
     bot.owner_id = setting["OWNER_ID"]
     async with bot:
