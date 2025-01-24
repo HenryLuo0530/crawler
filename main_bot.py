794931a83242355e805a18788ddd507f387b84b5
HenryLuo0530 <henryluo.tw@gmail.com>
2025-01-25 00:06:19 +0800

    extract log related code and add sys path

--- main_bot.py
+++ main_bot.py
@@ -1,12 +1,14 @@
+import sys
 import os
 import logging
-from logging.handlers import RotatingFileHandler
+import asyncio
 
 import discord
 from discord.ext import commands
-import asyncio
 
-from extension import tools
+from extension import tools, bot_logger
+
+sys.path.append('.')
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -14,22 +16,7 @@ logger = logging.getLogger(__name__)
 
 @bot.event
 async def on_ready():
-    is_normal_shutdown: bool
-    try:
-        with open("last_log.log", "r", encoding="utf-8") as log_file:
-            for line in log_file:
-                pass
-            last_log = line
-            if "[I] Bot has been shutted down" in last_log:
-                is_normal_shutdown = True
-            else:
-                is_normal_shutdown = False
-    except OSError:
-        logger.warning("[W] last_log not found")
-        is_normal_shutdown = True
-    except Exception as error:
-        logger.exception(error)
-        is_normal_shutdown = True
+    is_normal_shutdown = bot_logger.check_shutdown(logger)
 
     setting = tools.get_setting()
     quote = tools.get_quote()
@@ -47,26 +34,7 @@ async def load():
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
 async def main():
-    try:
-        os.replace("log.log", "last_log.log")
-    except OSError:
-        print("WARNING [W] log not found")
-    logger.setLevel(logging.INFO)
-    handler = RotatingFileHandler(
-        filename="log.log",
-        mode='a',
-        maxBytes=1024 * 2,
-        backupCount=0,
-        encoding='utf-8',
-        delay=False
-    )
-    handler.setLevel(logging.INFO)
-    logging.basicConfig(
-        format="%(name)s %(asctime)s %(levelname)s %(message)s",
-        level=logging.INFO,
-        datefmt="%Y-%m-%d %H:%M:%S",
-        handlers=[handler]
-    )
+    bot_logger.log_setup()
 
     setting = tools.get_setting()
     bot.owner_id = setting["OWNER_ID"]
